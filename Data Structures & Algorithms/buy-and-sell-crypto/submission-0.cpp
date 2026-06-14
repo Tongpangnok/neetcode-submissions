e0a42076f3d2374d382221ca58e1b6898bcc3fa0
Tongpangnok <tongpang231@gmail.com>
2026-06-14 18:52:37 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini=prices[0];
+        int maxi=0;
+        for(int i=1;i<prices.size();i++)
+        {
+            mini=min(mini,prices[i]);
+            int profit=prices[i]-mini;
+            maxi=max(maxi,profit);
+        }
+        return maxi;
+    }
+};
